0a1188c17926d4cdeeffb49e33866694dfd845d8
Andree Dantzler <andree.balayssac@icloud.com>
2021-02-06 20:50:17 -0500

    fixing config

--- config/connection.js
+++ config/connection.js
@@ -10,7 +10,7 @@ connection = mysql.createConnection(process.env.JAWSDB_URL);
   host: "localhost",
   port: 3306,
   user: "root",
-  password: "Dieuestbon1!",
+  password: "",
   database: "burgers_db"
   });
 };
